c9594636894603ac49cf83d3e6f10304e489fc1e
Marco Massarelli <60667061+ceoloide@users.noreply.github.com>
2024-04-09 21:04:53 +0000

    Upgrade ergogen version and remove `templates/index.js` patch

--- patch/patch_ergogen.sh
+++ patch/patch_ergogen.sh
@@ -7,16 +7,19 @@ fi
 if [ -d node_modules/ergogen ]; then
   echo "Patching Ergogen..."
   if [ -d node_modules/ergogen/src/footprints/ceoloide ]; then 
+    echo "Removing existing @ceoloide's footprint library"
     rm -rf node_modules/ergogen/src/footprints/ceoloide
   fi
   git clone https://github.com/ceoloide/ergogen-footprints.git node_modules/ergogen/src/footprints/ceoloide
   if [ -d node_modules/ergogen/src/footprints/infused-kim ]; then 
+    echo "Removing existing @infused-kim's footprint library"
     rm -rf node_modules/ergogen/src/footprints/infused-kim
   fi
   git clone https://github.com/infused-kim/kb_ergogen_fp.git node_modules/ergogen/src/footprints/infused-kim
   # Add the footprints to the index
+  echo "Patching `footprints/index.js`..."
   cp -f patch/footprints_index.js node_modules/ergogen/src/footprints/index.js
-  # Add KiCad 8 PCB template to the index and make it the default
-  cp -f patch/templates_index.js node_modules/ergogen/src/templates/index.js
+  # Make KiCad 8 the default
+  echo "Patching `pcbs.js`..."
   sed -i -e 's/kicad5/kicad8/g' node_modules/ergogen/src/pcbs.js
 fi
--- patch/templates_index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  kicad5: require('./kicad5'),
-  kicad8: require('./kicad8'),
-}
