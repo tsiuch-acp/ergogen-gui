430893b4c7791b35f30b7bf7dd4ce9b171951191
Marco Massarelli <marco.massarelli@gmail.com>
2024-04-14 22:33:27 -0400

    Update Ergogen to latest dev version

--- public/dependencies/ergogen.js
+++ public/dependencies/ergogen.js
@@ -647,11 +647,12 @@
 	        const list = [val];
 	        while (candidates.length) {
 	            const path = candidates.shift();
-	            const other = u$5.deepcopy(u$5.deep(root, path));
+	            const other = u$5.deep(root, path);
 	            a$7.assert(other, `"${path}" (reached from "${breadcrumbs.join('.')}.$extends") does not name a valid inheritance target!`);
 	            let parents = other.$extends || [];
 	            if (a$7.type(parents)() !== 'array') parents = [parents];
 	            candidates = candidates.concat(parents);
+	            a$7.assert(!list.includes(other), `"${path}" (reached from "${breadcrumbs.join('.')}.$extends") leads to a circular dependency!`);
 	            list.unshift(other);
 	        }
 	        val = extend.apply(null, list);
@@ -4483,7 +4484,7 @@
 	  params: {
 	    designator: 'MCU',
 	    side: 'F',
-	    reversible: true,
+	    reversible: false,
 	    reverse_mount: false,
 	    include_traces: true,
 	    include_extra_pins: false,
@@ -4988,6 +4989,109 @@
     )
   )
   `
+	};
+
+	// Copyright (c) 2023 Marco Massarelli
+	//
+	// SPDX-License-Identifier: CC-BY-NC-SA-4.0
+	//
+	// To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
+	//
+	// Author: @infused-kim + @ceoloide improvements
+	//
+	// Description:
+	//  A simple mounting hole with plated rim.
+	//
+	//  Note that some fine details may be lost depending on scale and fab capabilities.
+	//
+	// Params:
+	//    side: default is F for Front
+	//      The side on which to place the mounting hole.
+	//    outline: default is 0.8 (mm)
+	//      The width of the gold rim around the hole, in mm.
+	//    drill: default is 2.2 (mm / M2 screw)
+	//      The actual size for the hole. If drill_y is defined, it represents the
+	//      horizontal width of an oval hole, in mm.
+	//    drill_y: default is 0 (mm)
+	//      The vertical height of an oval hole, in mm.
+	//    include_courtyard: default is true
+	//      if true it will include the part courtyard
+	//
+	//  @ceoloide's improvements:
+	//    - Upgrade to KiCad 8
+	//    - Minor footprint restructure
+
+	var mounting_hole_plated = {
+	  params: {
+	    designator: 'H',
+	    side: 'F',
+	    outline: 0.8,
+	    drill: 2.2,
+	    drill_y: 0,
+	    include_courtyard: true,
+	  },
+	  body: p => {
+	    if (p.drill_y == 0) {
+	      p.drill_y = p.drill;
+	    }
+
+	    const size_x = p.drill + p.outline * 2;
+	    const size_y = p.drill_y + p.outline * 2;
+
+	    const courtyard_offset = 0.25;
+	    const courtyard_x = size_x / 2 + courtyard_offset;
+	    const courtyard_y = size_y / 2 + courtyard_offset;
+
+	    const top = `
+  (footprint "ceoloide:mounting_hole_plated"
+    (layer "${p.side}.Cu")
+    ${p.at}
+    (property "Reference" "${p.ref}"
+      (at 0 3 ${p.r})
+      (layer "${p.side}.SilkS")
+      ${p.ref_hide}
+      (effects (font (size 1 1) (thickness 0.15)))
+    )
+    `;
+
+	    const pad_circle = `
+    (pad "" thru_hole circle (at 0 0 ${p.r}) (size ${size_x} ${size_y}) (drill ${p.drill}) (layers "*.Cu" "*.Mask"))
+    `;
+	    const courtyard_circle = `
+    (fp_circle (center 0 0) (end -${courtyard_x} 0) (layer "F.CrtYd") (stroke (width 0.05) (type solid)) (fill none))
+    (fp_circle (center 0 0) (end -${courtyard_x} 0) (layer "B.CrtYd") (stroke (width 0.05) (type solid)) (fill none))
+    `;
+	    const pad_oval = `
+    (pad "" thru_hole oval (at 0 0 ${p.r}) (size ${size_x} ${size_y}) (drill oval ${p.drill} ${p.drill_y}) (layers "*.Cu" "*.Mask"))
+    `;
+	    const courtyard_oval = `
+    (fp_line (start ${courtyard_x} -${courtyard_y}) (end ${courtyard_x} ${courtyard_y}) (layer "F.CrtYd") (stroke (width 0.05) (type solid)))
+    (fp_line (start -${courtyard_x} -${courtyard_y}) (end -${courtyard_x} ${courtyard_y}) (layer "F.CrtYd") (stroke (width 0.05) (type solid)))
+    (fp_line (start -${courtyard_x} ${courtyard_y}) (end ${courtyard_x} ${courtyard_y}) (layer "F.CrtYd") (stroke (width 0.05) (type solid)))
+    (fp_line (start -${courtyard_x} -${courtyard_y}) (end ${courtyard_x} -${courtyard_y}) (layer "F.CrtYd") (stroke (width 0.05) (type solid)))
+    (fp_line (start -${courtyard_x} ${courtyard_y}) (end ${courtyard_x} ${courtyard_y}) (layer "B.CrtYd") (stroke (width 0.05) (type solid)))
+    (fp_line (start -${courtyard_x} ${courtyard_y}) (end -${courtyard_x} -${courtyard_y}) (layer "B.CrtYd") (stroke (width 0.05) (type solid)))
+    (fp_line (start -${courtyard_x} -${courtyard_y}) (end ${courtyard_x} -${courtyard_y}) (layer "B.CrtYd") (stroke (width 0.05) (type solid)))
+    (fp_line (start ${courtyard_x} ${courtyard_y}) (end ${courtyard_x} -${courtyard_y}) (layer "B.CrtYd") (stroke (width 0.05) (type solid)))
+    `;
+
+	    const bottom = `
+  )
+    `;
+
+	    let final = top;
+	    if (size_x == size_y) {
+	      final += pad_circle;
+	      final += courtyard_circle;
+	    } else {
+	      final += pad_oval;
+	      final += courtyard_oval;
+	    }
+
+	    final += bottom;
+
+	    return final
+	  }
 	};
 
 	// Copyright (c) 2023 Marco Massarelli
@@ -8693,6 +8797,7 @@
 	  'ceoloide/led_sk6812mini-e': led_sk6812miniE,
 	  'ceoloide/mcu_nice_nano': mcu_nice_nano,
 	  'ceoloide/mounting_hole_npth': mounting_hole_npth,
+	  'ceoloide/mounting_hole_plated': mounting_hole_plated,
 	  'ceoloide/power_switch_smd_side': power_switch_smd_side,
 	  'ceoloide/reset_switch_smd_side': reset_switch_smd_side,
 	  'ceoloide/reset_switch_tht_top': reset_switch_tht_top,
